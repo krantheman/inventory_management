29e85db86df12510a42510473f479b40fb4c9221
Akash Tom <atom@Venturer.local>
2023-07-12 13:50:23 +0530

    feat: item validation

--- inventory_management/inventory_management/doctype/item/item.py
+++ inventory_management/inventory_management/doctype/item/item.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2023, krantheman and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Item(Document):
-	pass
+    def before_save(self):
+        receipt = self.opening_warehouse or self.opening_rate or self.opening_qty
+        if receipt:
+            self.validate_receipt()
+
+    def validate_receipt(self):
+        self.validate_field(self.opening_warehouse, "warehouse")
+        self.validate_field(self.opening_qty, "quantity")
+        self.validate_field(self.opening_rate, "valuation rate")
+
+    def validate_field(self, field, str_field):
+        if not field:
+            frappe.throw("Enter valid opening {} for receipt".format(str_field))
