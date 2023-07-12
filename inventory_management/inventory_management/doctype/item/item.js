6d0be86ef6f1e118b7c3cdc8b1558c42daae7d30
Akash Tom <atom@Akashs-MacBook-Air.local>
2023-07-12 10:57:59 +0530

    feat: add doctypes

---
+++ inventory_management/inventory_management/doctype/item/item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, krantheman and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Item", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ inventory_management/inventory_management/doctype/warehouse/warehouse.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, krantheman and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Warehouse(Document):
+	pass
