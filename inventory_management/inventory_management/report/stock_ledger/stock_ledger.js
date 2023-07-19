d68b77dadf249f68eff980906c09b3136e47a79b
krantheman <akash.akasht.tom@gmail.com>
2023-07-19 23:29:13 +0530

    feat(Stock Ledger Report): add filters

--- inventory_management/inventory_management/report/stock_ledger/stock_ledger.js
+++ inventory_management/inventory_management/report/stock_ledger/stock_ledger.js
@@ -2,7 +2,46 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Stock Ledger"] = {
-	"filters": [
-
-	]
+  filters: [
+    {
+      fieldname: "from_date",
+      label: __("From"),
+      fieldtype: "Date",
+    },
+    {
+      fieldname: "to_date",
+      label: __("To"),
+      fieldtype: "Date",
+    },
+    ,
+    {
+      fieldname: "item",
+      label: __("Item"),
+      fieldtype: "Link",
+      options: "Item",
+    },
+    {
+      fieldname: "warehouse",
+      label: __("Warehouse"),
+      fieldtype: "Link",
+      options: "Warehouse",
+    },
+    {
+      fieldname: "type",
+      label: __("Type"),
+      fieldtype: "Select",
+      options: ["", "Incoming", "Outgoing"],
+    },
+  ],
+  formatter: (value, row, column, data, default_formatter) => {
+    if (
+      column.fieldname === "qty_change" ||
+      column.fieldname === "value_change"
+    ) {
+      value = default_formatter(value, row, column, data);
+      const color = value >= 0 ? "green" : "red";
+      value = `<span style='color:${color}!important'>${value}</span>`;
+    }
+    return value;
+  },
 };
--- inventory_management/inventory_management/report/stock_ledger/stock_ledger.py
+++ inventory_management/inventory_management/report/stock_ledger/stock_ledger.py
@@ -26,16 +26,10 @@ def execute(filters=None):
             "options": "Warehouse",
         },
         {
-            "fieldname": "qty_in",
-            "label": "Quantity In",
+            "fieldname": "qty_change",
+            "label": "Quantity Change",
             "fieldtype": "int",
-            "options": "Quantity In",
-        },
-        {
-            "fieldname": "qty_out",
-            "label": "Quantity Out",
-            "fieldtype": "int",
-            "options": "Quantity Out",
+            "options": "Quantity Change",
         },
         {
             "fieldname": "balance_qty",
@@ -63,34 +57,53 @@ def execute(filters=None):
         },
     ]
 
-    entries = frappe.db.get_all(
+    data = frappe.db.get_all(
         "Stock Ledger Entry",
         ["date", "time", "item", "warehouse", "qty_change", "valuation_rate"],
-        filters,
         order_by="date",
+        filters=format_filters(filters),
     )
-    update_entries(entries)
+    format_data(data)
 
-    return map(lambda x: {**x, "width": 120}, columns), entries
+    return map(lambda x: {**x, "width": 134}, columns), data
 
 
-def update_entries(entries):
+def format_data(data):
     balance_qty = {}
     balance_value = {}
-    for entry in entries:
-        entry.time = str(entry.time).split(".")[0]
-        entry.qty_in = entry.qty_change if entry.qty_change > 0 else 0
-        entry.qty_out = entry.qty_change if entry.qty_change < 0 else 0
-        entry.value_change = entry.qty_change * entry.valuation_rate
-        key = f"{entry.item}-{entry.warehouse}"
+    for row in data:
+        row.time = str(row.time).split(".")[0]  # remove milliseconds
+        row.value_change = row.qty_change * row.valuation_rate
+        key = f"{row.item}-{row.warehouse}"
         if key in balance_qty:
-            balance_qty[key] += entry.qty_change
-            balance_value[key] += entry.value_change
+            balance_qty[key] += row.qty_change
+            balance_value[key] += row.value_change
         else:
-            balance_qty[key] = entry.qty_change
-            balance_value[key] = entry.value_change
-        entry.balance_qty = balance_qty[key]
-        entry.balance_value = balance_value[key]
-        entry.valuation_rate = "{:.2f}".format(entry.valuation_rate)
-        entry.value_change = "{:.2f}".format(entry.value_change)
-        entry.balance_value = "{:.2f}".format(entry.balance_value)
+            balance_qty[key] = row.qty_change
+            balance_value[key] = row.value_change
+        row.balance_qty = balance_qty[key]
+        row.balance_value = balance_value[key]
+        # Format to two decimal places
+        row.valuation_rate = "{:.2f}".format(row.valuation_rate)
+        row.value_change = "{:.2f}".format(row.value_change)
+        row.balance_value = "{:.2f}".format(row.balance_value)
+
+
+def format_filters(filters):
+    from_date = "1900-01-01"
+    to_date = "2100-01-01"
+    applied_filters = {}
+    if filters.from_date:
+        from_date = filters.from_date
+    if filters.to_date:
+        to_date = filters.to_date
+    applied_filters["date"] = ["between", [from_date, to_date]]
+    if filters.item:
+        applied_filters["item"] = filters.item
+    if filters.warehouse:
+        applied_filters["warehouse"] = filters.warehouse
+    if filters.type == "Incoming":
+        applied_filters["qty_change"] = [">", 0]
+    elif filters.type == "Outgoing":
+        applied_filters["qty_change"] = ["<", 0]
+    return applied_filters
